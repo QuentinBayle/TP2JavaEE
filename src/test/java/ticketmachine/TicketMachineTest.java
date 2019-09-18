870dd6fbe0cbb5dd29e4ab05fac18305473f99ad
QuentinBayle <quentin.bayle@etud.univ-jfc.fr>
2019-09-18 15:17:58 +0200

    sujetTp2

---
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -0,0 +1,33 @@
+package ticketmachine;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TicketMachineTest {
+	private static final int PRICE = 50; // Une constante
+
+	private TicketMachine machine; // l'objet à tester
+
+	@Before
+	public void setUp() {
+		machine = new TicketMachine(PRICE); // On initialise l'objet à tester
+	}
+
+	@Test
+	// On vérifie que le prix affiché correspond au paramètre passé lors de l'initialisation
+	// S1 : le prix affiché correspond à l’initialisation
+	public void priceIsCorrectlyInitialized() {
+		// Paramètres : message si erreur, valeur attendue, valeur réelle
+		assertEquals("Initialisation incorrecte du prix", PRICE, machine.getPrice());
+	}
+
+	@Test
+	// S2 : la balance change quand on insère de l’argent
+	public void insertMoneyChangesBalance() {
+		machine.insertMoney(10);
+		machine.insertMoney(20);
+		assertEquals("La balance n'est pas correctement mise à jour", 10 + 20, machine.getBalance()); // Les montants ont été correctement additionnés               
+	}
+
+}
